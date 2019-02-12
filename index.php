d061161019197b0dad4b32885334964222b83116
zedbpro <zedblqpam@gmail.com>
2019-02-12 22:09:18 +0100

    bew

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+
+$to = "kralouat@gmail.com";
+$message = "taha is the best";
+$subject = "kraloua is the best";
+$headers = "Frome: taha";
+
+
+mail ($to ,$subject ,$message,$headers );
+
+ ?>
